2c25b48809a5d0141237fddab882309f078d5a74
FaroukJGRNT <farouk.okanla@epitech.eu>
2025-07-22 12:33:50 +0100

    fix: the program now creates the schemes in the db after connexion

--- config/db.js
+++ config/db.js
@@ -10,4 +10,23 @@ const connexion = mysql.createConnection({
   password: process.env["PASSWORD"]
 });
 
+//Create the schemes
+connexion.query(`CREATE TABLE IF NOT EXISTS user (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    firstname VARCHAR(255) NOT NULL,
+    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);`)
+connexion.query(`CREATE TABLE IF NOT EXISTS todo (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    due_time DATETIME NOT NULL,
+    status ENUM("todo", "in progress", "done") NOT NULL DEFAULT "todo",
+    user_id INT NOT NULL
+);`)
+
 module.exports = { connexion }
